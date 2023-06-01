9a78dac7559bcfd53038288373d00a97ba6b13d1
vD-Evan <vanduzee@sheridancollege.ca>
2023-05-31 23:32:11 -0400

    Fix: Variable names; Add: Comments

--- src/main/java/ca/sheridan/syst17796/groupdeliverable/card/CreateCard.java
+++ src/main/java/ca/sheridan/syst17796/groupdeliverable/card/CreateCard.java
@@ -1,34 +1,34 @@
 package ca.sheridan.syst17796.groupdeliverable.card;
 
 public class CreateCard {
-    private int iSuit;
-    private int iValue;
+    private int suitAsInt;
+    private int valueAsInt;
 
-    public CreateCard(int iSuit, int iValue) {
-        this.iSuit = iSuit;
-        this.iValue = iValue;
+    public CreateCard(int suitAsInt, int valueAsInt) {
+        this.suitAsInt = suitAsInt;
+        this.valueAsInt = valueAsInt;
     }
 
-    public int getiSuit() {
-        return iSuit;
+    public int getSuitAsString() {
+        return suitAsInt;
     }
 
-    public int getiValue() {
-        return iValue;
+    public int getValueAsInt() {
+        return valueAsInt;
     }
 
-    public void setiSuit(int iSuit) {
-        this.iSuit = iSuit;
+    public void setSuitAsInt(int suitAsInt) {
+        this.suitAsInt = suitAsInt;
     }
 
-    public void setiValue(int iValue) {
-        this.iValue = iValue;
+    public void setValueAsInt(int valueAsInt) {
+        this.valueAsInt = valueAsInt;
     }
 
     @Override
     public String toString() {
-        String sSuit = CardUtils.getSuitString(iSuit);
-        String sValue = CardUtils.getRankString(iValue);
-        return sValue + " of " + sSuit;
+        String suitAsString = CardUtils.getSuitString(suitAsInt);
+        String valueAsString = CardUtils.getRankString(valueAsInt);
+        return valueAsString + " of " + suitAsString;
     }
 }
