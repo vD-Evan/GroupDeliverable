06b7a7c19516ef96aa2f27dc7a81945d2e793eb0
vD-Evan <vanduzee@sheridancollege.ca>
2023-05-31 23:01:52 -0400

    Add: Comments; Fix: Variable Names

--- src/main/java/ca/sheridan/syst17796/groupdeliverable/misc/Player.java
+++ src/main/java/ca/sheridan/syst17796/groupdeliverable/misc/Player.java
@@ -1,39 +1,44 @@
 package ca.sheridan.syst17796.groupdeliverable.misc;
 
-import java.util.ArrayList;
 import ca.sheridan.syst17796.groupdeliverable.card.CreateCard;
+import java.util.ArrayList;
 
 public class Player {
-    private String sName;
-    private ArrayList<CreateCard> alHand;
-    private boolean decidedToStand;
+    private String name;
+    private ArrayList<CreateCard> hand; // Array List for dynamically created hands rather than a hardcap of 52
+    private boolean decidedToStand; // Variable for Black Jack; Recommended to rename later if another game has a
+                                    // similarly named mechanic
 
-    public Player(String sName) {
-        this.sName = sName;
-        this.alHand = new ArrayList<>();
-        this.decidedToStand = false;
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<>();
+        this.decidedToStand = false; // Black Jack
     }
 
     public String getName() {
-        return sName;
+        return name;
     }
 
-    public void addCard(CreateCard objCard) {
-        alHand.add(objCard);
+    public void addCard(CreateCard card) {
+        hand.add(card); // Syntax for adding a new object at end of Array List
     }
 
     public ArrayList<CreateCard> getHand() {
-        return alHand;
+        return hand; // Array List is passed as a variable rather than as an array (No need for
+                     // square brackets)
     }
 
-    public int getHandValue() {
+    public int getHandValue() { // Originally made for Black Jack, this code is redundant (Black Jack has
+                                // game-specific rules) and remains to be used in other games
         int value = 0;
-        for (CreateCard objCard : alHand) {
-            value += objCard.getiValue();
+        for (CreateCard card : hand) {
+            value += card.getValueAsInt(); // syntax for accessing an object's properties (read: variables) when object
+                                           // is stored in an array
         }
         return value;
     }
 
+    // Below is for Black Jack; similar recommendation as above
     public boolean hasDecidedToStand() {
         return decidedToStand;
     }
